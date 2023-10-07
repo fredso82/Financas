da657b23a7fc61d338724db14a6a7f769aefbf73
fredso82 <fredso82@gmail.com>
2023-10-06 22:40:59 -0300

    Tradução do calendário

--- src/frontend/src/app/app.component.ts
+++ src/frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MenuItem } from 'primeng/api';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
     selector: 'app-root',
@@ -12,4 +13,28 @@ export class AppComponent {
         { label: 'Categorias', icon: 'pi pi-fw pi-tags', routerLink: ['/categorias'] },
         { label: 'Formas de Pagamento', icon: 'pi pi-fw pi-credit-card', routerLink: ['/formas-de-pagamento'] }
     ];
+
+    constructor(private primengConfig: PrimeNGConfig){
+
+    }
+
+    ngOnInit() {
+        this.primengConfig.ripple = true;
+        this.primengConfig.setTranslation({
+            "apply": "Aplicar",
+            "accept": "Sim",
+            "reject": "Não",
+            "cancel": "Cancelar",
+            "dayNames": ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
+            "dayNamesShort": ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"],
+            "dayNamesMin": ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"],
+            "monthNamesShort": ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun","Jul", "Ago", "Set", "Out", "Nov", "Dez"],
+            "monthNames": ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"],
+            "dateFormat": "dd/mm/yy",
+            "today": "Hoje",
+            "passwordPrompt": "Digite uma senha",
+            "emptyMessage":	"Nenhum registro encontrado",
+            "emptyFilterMessage": "Nenhum registro encontrado"
+        });
+    }
 }
