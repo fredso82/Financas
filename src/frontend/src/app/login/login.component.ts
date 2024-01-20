45189b5fc9b698d6309709fe9351a083fec80c7e
fredso82 <fredso82@gmail.com>
2024-01-20 17:35:49 -0300

    Tela de login

--- src/frontend/src/app/login/login.component.ts
+++ src/frontend/src/app/login/login.component.ts
@@ -1,27 +1,43 @@
 import { Component, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { Router } from '@angular/router';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, InputTextModule, ButtonModule],
+  imports: [FormsModule, InputTextModule, ButtonModule, ReactiveFormsModule, CardModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
     router = inject(Router);
-    login = "";
-    senha = "";
+    loginForm!: FormGroup;
 
-    entrar() : void {
-        if (this.login == "admin" && this.senha && "admin") {
-            this.router.navigateByUrl("/");
-            return;
-        }
-        alert("Login ou senha inválidos");
+    constructor(private fb: FormBuilder){
+        this.loginForm = this.fb.group({
+            login: ['', [Validators.required]],
+            senha: ['', [Validators.required]]
+        });
     }
+
+    onSubmit() {
+        if (this.loginForm.valid){
+            if (this.loginForm.value.login == "admin" && this.loginForm.value.senha == "admin") {
+                this.router.navigateByUrl("/");
+                return;
+            }
+            alert("Login ou senha inválidos");
+        } 
+    }
+    // entrar() : void {
+    //     if (this.login == "admin" && this.senha && "admin") {
+    //         this.router.navigateByUrl("/");
+    //         return;
+    //     }
+    //     alert("Login ou senha inválidos");
+    // }
 }
 
