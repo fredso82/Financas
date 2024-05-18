997bfaa4ee4750462128f23b1d1b2c24a07c838b
fredso82 <fredso82@gmail.com>
2024-05-18 11:24:07 -0300

    Transformando app Categroria para usar signals

--- src/frontend/src/app/categoria/alteracao/alteracao.component.ts
+++ src/frontend/src/app/categoria/alteracao/alteracao.component.ts
@@ -1,20 +1,29 @@
-import { Component, EventEmitter, Input, OnInit, Output, input, output } from '@angular/core';
-import { Categoria } from '../models/categoria';
-import { CategoriaService } from '../categoria.service';
 import { NgClass } from '@angular/common';
-import { InputTextModule } from 'primeng/inputtext';
+import { Component, input, OnInit, output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+
+import { CategoriaService } from '../categoria.service';
+import { Categoria } from '../models/categoria';
 
 @Component({
     selector: 'app-catagoria-alteracao',
-    templateUrl: './alteracao.component.html',
     standalone: true,
-    imports: [FormsModule, InputTextModule, NgClass]
+    imports: [FormsModule, InputTextModule, NgClass],
+    template: `
+        <div class="field">
+            <label for="nome">Nome <span class="obrigatorio">*</span></label>
+            <input type="text" pInputText [(ngModel)]="categoria().nome" required autofocus id="nome" [ngClass]="{ 'ng-invalid ng-dirty': submitted() && !categoria().nome }" />
+            @if (submitted() && !categoria().nome) {
+                <small class="p-error block">Informe o nome</small>
+            }
+        </div>
+    `
 })
 export class AlteracaoCategoriaComponent implements OnInit {
     onSave = output<{ sucesso: boolean, dados: any }>();
     categoria = input<Categoria>({id: "", nome: ""});
-    submitted = false;
+    submitted = signal(false);
     
     constructor(private categoriaService: CategoriaService) {     
     }
@@ -23,17 +32,13 @@ export class AlteracaoCategoriaComponent implements OnInit {
     }
 
     gravar() {
-        this.submitted = true;
+        this.submitted.set(true);
         if (!this.categoria().nome.trim())
             return;
 
         this.categoriaService.alterar(this.categoria()).subscribe({
-            next: (e) => {
-                this.onSave.emit({ sucesso: true, dados: this.categoria });
-            },
-            error: (e) => {
-                this.onSave.emit({ sucesso: false, dados: e});
-            }
+            next: (e) => { this.onSave.emit({ sucesso: true, dados: this.categoria }) },
+            error: (e) => { this.onSave.emit({ sucesso: false, dados: e}) }
         });
     }
 }
--- src/frontend/src/app/categoria/novo/novo.component.ts
+++ src/frontend/src/app/categoria/novo/novo.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, signal } from '@angular/core';
 import { Categoria } from '../models/categoria';
 import { CategoriaService } from '../categoria.service';
 import { NgClass } from '@angular/common';
@@ -8,36 +8,40 @@ import { output } from '@angular/core';
 
 @Component({
     selector: 'app-categoria-novo',
-    templateUrl: './novo.component.html',
     standalone: true,
-    imports: [FormsModule, InputTextModule, NgClass]
+    imports: [FormsModule, InputTextModule, NgClass],
+    template: `
+        <div class="field">
+            <label for="nome">Nome <span class="obrigatorio">*</span></label>
+            <input type="text" pInputText [(ngModel)]="categoria().nome" required autofocus id="nome" 
+                [ngClass]="{ 'ng-invalid ng-dirty': submitted() && !categoria().nome }" />
+                
+            @if (submitted() && !categoria().nome) {
+                <small class="p-error block">Informe o nome</small>
+            }
+        </div>
+    `
 })
 export class NovaCategoriaComponent implements OnInit {
     onSave = output<{ sucesso: boolean, dados: any }>();
 
-    categoria: Categoria;
-    submitted = false;
+    categoria = signal<Categoria>({ id: "", nome: "" });
+    submitted = signal(false);
 
     constructor(private categoriaService: CategoriaService) {
-        this.categoria = { id: "", nome: "" };
+        this.categoria.set({ id: "", nome: "" });
     }
-    ngOnInit(): void {
-
-    }
+    
+    ngOnInit(): void {}
 
     gravar() {
-        this.submitted = true;
-        if (!this.categoria.nome.trim())
+        this.submitted.set(true);
+        if (!this.categoria().nome.trim())
             return;
 
-        this.categoriaService.incluir(this.categoria).subscribe({
-            next: (e) => {
-                this.onSave.emit({ sucesso: true, dados: this.categoria });
-            },
-            error: (e) => {
-                this.onSave.emit({ sucesso: false, dados: e});
-            }
+        this.categoriaService.incluir(this.categoria()).subscribe({
+            next: (e) => { this.onSave.emit({ sucesso: true, dados: this.categoria })},
+            error: (e) => { this.onSave.emit({ sucesso: false, dados: e })}
         });
     }
-
 }
